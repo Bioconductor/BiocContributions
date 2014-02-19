bf3a6cd8109ccad28b8f099cb297448e78534ccb
m.carlson <m.carlson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-19 00:16:05 +0000

    Adds three more utility functions for reviewing packages.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@86561 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tallyManifests.R
+++ R/tallyManifests.R
@@ -1,26 +1,40 @@
 ## Used to count and plot the amount of packages in the respos.
-## path <- "~/proj/Rpacks/"
 
-## range <- 6:9
-## manis1 <- paste0(path, "bioc_1.",range,".manifest")
-## range <- 0:14
-## manis2 <- paste0(path, "bioc_2.",range,".manifest")
-## manis <- c(manis1, manis2)
+## helper that generates names for manifests.  You have to update this
+## each release in order to get the current totals!
+.makeManifestNames <- function(path){
+    range1 <- 6:9
+    manis1 <- paste0(path, "bioc_1.",range1,".manifest")
+    range2 <- 0:14
+    manis2 <- paste0(path, "bioc_2.",range2,".manifest")
+    c(manis1, manis2)
+}
 
-## scanMani <- function(file){
-##     res <- scan(file, what="character",skip=1)
-##     table(grepl("Package", res))[["TRUE"]]
-## }
-
-## scans <- unlist(lapply(manis, scanMani))
-
-## plot(scans)
-
-## ## line if the slope was twenty (which is about what we started at
-## abline(a=100,b=20,col="red")
+## Helper to just read in one manifest and get the total number of packages.
+.scanMani <- function(file){
+    res <- scan(file, what="character",skip=1, quiet=TRUE)
+    table(grepl("Package", res))[["TRUE"]]
+}
 
 
-## TODO: Make a function that makes the plot, and another function
-## that makes just a named vector with the counts for each release.
+## This extracts the package totals based on existing manifests
+getPackageTotals <- function(path = "~/proj/Rpacks/"){
+    manis <- .makeManifestNames(path)
+    maniNames <- .makeManifestNames("")
+    setNames(unlist(lapply(manis, .scanMani)), maniNames)
+}
+
+## getPackageTotals()
+
+
+## And this plots the package totals based on existing manifests
+plotPackageTotals <- function(path = "~/proj/Rpacks/"){
+    totals <- getPackageTotals()
+    plot(totals)
+    abline(a=100,b=20,col="red")
+}
+
+## plotPackageTotals()
+
 
 
