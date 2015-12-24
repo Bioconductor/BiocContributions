4fb5dd3cf637ae25f9137e48b98d7bd77815d338
j.hester <j.hester@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-12-24 18:33:33 +0000

    Add add_to_spreadsheet function
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@111908 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/reporting.R
@@ -0,0 +1,24 @@
+tracker_spreadsheet <- function(x) {
+  if (is.null(the$ss)) {
+      the$ss <- gs_title("Bioconductor Pkg Review")
+  }
+  the$ss
+}
+
+#' Add packages to the tracking worksheet
+#'
+#' This function uses the googlesheets package to add data retrieved from the
+#' tracker.
+#' @param x issues to add
+#' @param ws The worksheet number or name to append to.
+#' @param ... Additional arguments passed to \code{\link[googlesheets]{gs_add_row}}
+add_to_spreadsheet <- function(x, ..., ws = 1, ss = tracker_spreadsheet()) {
+    data <- data.frame("Package to be added" = x$title,
+        "Issue" = paste0("https://tracker.bioconductor.org/issue", x$id),
+        "Github" = "",
+        "accepted on tracker" = x$activity,
+        "type of package" = vapply(x$keyword, paste, collapse = ", ", character(1)),
+        check.names = FALSE,
+        stringsAsFactors = FALSE)
+    gs_add_row(apply(data, 1, gs_add_row, ss = tracker_spreadsheet(), ws = ws, ...))
+}
