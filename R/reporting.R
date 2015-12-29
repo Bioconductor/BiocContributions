acf788a22dce79efaf40607ca5645f5c56ad9299
j.hester <j.hester@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-12-29 19:37:40 +0000

    Export add_to_spreadsheet
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@111999 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/reporting.R
+++ R/reporting.R
@@ -1,6 +1,6 @@
 tracker_spreadsheet <- function(x) {
   if (is.null(the$ss)) {
-      the$ss <- gs_title("Bioconductor Pkg Review")
+      the$ss <- googlesheets::gs_title("Bioconductor Pkg Review")
   }
   the$ss
 }
@@ -12,6 +12,12 @@ tracker_spreadsheet <- function(x) {
 #' @param x issues to add
 #' @param ws The worksheet number or name to append to.
 #' @param ... Additional arguments passed to \code{\link[googlesheets]{gs_add_row}}
+#' @export
+#' @examples
+#' \dontrun{
+#' pkgs <- unassigned_packages()
+#' add_to_spreadsheet(pkgs)
+#' }
 add_to_spreadsheet <- function(x, ..., ws = 1, ss = tracker_spreadsheet()) {
     data <- data.frame("Package to be added" = x$title,
         "Issue" = paste0("https://tracker.bioconductor.org/issue", x$id),
@@ -20,5 +26,5 @@ add_to_spreadsheet <- function(x, ..., ws = 1, ss = tracker_spreadsheet()) {
         "type of package" = vapply(x$keyword, paste, collapse = ", ", character(1)),
         check.names = FALSE,
         stringsAsFactors = FALSE)
-    gs_add_row(apply(data, 1, gs_add_row, ss = tracker_spreadsheet(), ws = ws, ...))
+    apply(data, 1, googlesheets::gs_add_row, ss = tracker_spreadsheet(), ws = ws, ...)
 }
