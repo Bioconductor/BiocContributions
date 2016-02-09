85156e4d64477d35b3327d9228969048882af354
j.java <j.java@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-02-09 16:08:47 +0000

    Better interaction with gmailr; version bump.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@113446 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/simplify.R
+++ R/simplify.R
@@ -1,16 +1,16 @@
-CreatePackageAssignmentEmail <- function(assignInTracker=FALSE)
+CreatePackageAssignmentEmail <- function(assignInTracker=FALSE, secret="~/bioconductorseattle-gmail.json")
 {
     ## Retrieve unassigned packages
     pkgs <- unassigned_packages()
-    
+
     ## Generate code used for assigning packages
     code <- assign_new_packages(pkgs)
-    
+
     ## Generate package assignment email
     email <- package_assignment_email(pkgs, code)
-    
+
     ## Create a new draft email with assignment content (in Gmail drafts)
-    gmailr::gmail_auth(scope="compose")
+    gmailr::gmail_auth(scope="compose", secret_file=secret)
     gmailr::create_draft(email)
 
     if (assignInTracker) {
@@ -19,5 +19,4 @@ CreatePackageAssignmentEmail <- function(assignInTracker=FALSE)
     } else
         return (list(pkgs=pkgs, code=code))
 }
-
 
