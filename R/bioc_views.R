e3a6c9c5d166905cf8a0bb68872245b9462a4a70
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-05 20:20:30 +0000

    feature: bioc_views_classification
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@114276 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/bioc_views.R
@@ -0,0 +1,39 @@
+bioc_views_from_tarball <- function(files) {
+    names(files) <- basename(files)
+    descriptions <- lapply(files, readDESCRIPTION)
+    views <- vapply(descriptions, `[[`, character(1), "biocViews")
+    strsplit(views, "[[:blank:],\n]+")
+}
+
+#' Extract and validiate biocViews terms from tarball
+#'
+#' @param files tar.gz file locations
+#' @return named vector classifying each tar ball to biocViews hierarchy
+#' @export
+bioc_views_classification <- function(files) {
+    views <- bioc_views_from_tarball (files)
+    curr <- biocViews::getCurrentbiocViews()
+    curr <- setNames(unlist(curr, use.names=FALSE),
+                     rep(names(curr), lengths(curr)))
+
+    idx <- match(unlist(views), curr)
+    if (anyNA(idx)) {
+        msg <- paste0(unlist(views)[is.na(idx)], " (",
+                      rep(names(views), lengths(views))[is.na(idx)],
+                      ")", collapse="\n    ")
+        stop("invalid biocViews:\n    ", msg)
+    }
+
+    class <- relist(names(curr)[idx], views)
+    class <- lapply(class, unique)
+
+    if (!all(lengths(class) == 1L)) {
+        msg <- paste(Map(function(nm, v, c) {
+            sprintf("%s: %s", nm, paste0(v, " (", c, ")", collapse=", "))
+        }, names(views)[!ok], views[!ok], class[!ok]), collapse="\n    ")
+        stop("invalid terms:\n    ", msg)
+    }
+
+    unlist(class)
+}
+
