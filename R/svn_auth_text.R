7e8e405f14c3ffc20e7a69df380f91f12d0922ac
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-12 20:56:41 +0000

    update R versoin
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocContributions@117379 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/svn_auth_text.R
+++ R/svn_auth_text.R
@@ -1,12 +1,22 @@
-##' Generate text to copy / paste into svn file Bioconductor.authz
+##' Update svn authz files
+##' 
+##' These functions generate text to copy / paste into software svn
+##' file bioconductor.authz or data experiment svn file
+##' bioc-data.authz
 ##'
 ##' @param filenames character() vector of (full) paths to package tar balls.
+##' 
 ##' @param version character(1) version string as it appears in svn
 ##'     'RELEASE' branch, e.g., \code{"3_2"} corresponding to
 ##'     RELEASE_3_2.
+##'
+##' @name svn_auth_text
+NULL
+
+##' @rdname svn_auth_text
 ##' @export
-svn_auth_text <-
-    function(filenames, version=getOption("BC_auth_version", "3_2"))
+svn_software_auth_text <-
+    function(filenames, version=getOption("BC_auth_version", "3_3"))
 {
     m <- lapply(filenames, maintainers)
     userid <- tolower(vapply(m, function(elt) {
@@ -23,10 +33,47 @@ svn_auth_text <-
 
     ## permission
     for (p in package)
-        cat(paste0("[/trunk/madman/Rpacks/", p, "]\n",
-                   "@", p, " = rw\n",
-                   "\n",
-                   "[/branches/RELEASE_", version, "/madman/Rpacks/", p, "]\n",
-                   "@", p, " = rw\n"),
+        cat(paste0(
+            "[/trunk/madman/Rpacks/", p, "]\n",
+            "@", p, " = rw\n",
+            "\n",
+            "[/branches/RELEASE_", version, "/madman/Rpacks/", p, "]\n",
+            "@", p, " = rw\n"),
+            "\n")
+}
+
+
+##' @rdname svn_auth_text
+##' @export
+svn_data_experiment_auth_text <-
+    function(filenames, version=getOption("BC_auth_version", "3_3"))
+{
+    m <- lapply(filenames, maintainers)
+    userid <- tolower(vapply(m, function(elt) {
+        elt <- elt[[1]]                 # first maintainer only
+        sprintf("%s.%s", substr(elt$given[1], 1, 1), elt$family)
+    }, ""))
+    package <- sub("_0.99.*", "", basename(filenames))
+
+    ## read permission
+    cat(paste(c("", userid), collapse=", "), "\n\n")
+
+    ## groups
+    cat(paste(package, userid, sep=" = ", collapse="\n"), "\n\n")
+
+    ## permission
+    for (p in package)
+        cat(paste0(
+            "[/trunk/experiment/pkgs/", p, "]\n",
+            "@", p, " = rw\n",
+            "\n",
+            "[/trunk/experiment/data_store/", p, "]\n",
+            "@", p, " = rw\n",
+            "\n",
+            "[/branches/RELEASE_", version, "/experiment/pkgs/", p, "]\n",
+            "@", p, " = rw\n",
+            "\n",
+            "[/branches/RELEASE_", version, "/experiment/data_store/", p, "]\n",
+            "@", p, " = rw\n"),
             "\n")
 }
