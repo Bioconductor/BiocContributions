2035196820465ddffcf1e6d588e255c26b295f3d
lshep <lori.shepherd@roswellpark.org>
2026-04-09 13:13:49 -0400

    Update wording

--- scripts/inactive_review.py
+++ scripts/inactive_review.py
@@ -190,6 +190,9 @@ This issue is being closed because there has been no progress for an extended pe
 
 You are welcome to reopen the issue when you are able to actively participate in the review and submission process. Please keep in mind that acceptance requires a commitment to ongoing package maintenance.
 
+Your package has been removed from the new submission location and registry. 
+If you reopen, please complete the precheck and policy acceptance again to reinitialize review process. 
+
 Thank you for your interest in Bioconductor.
 """
     post_comment(issue_number, closing_comment)
