5b099ae9ef6e773a679be313420d066572a9ebc0
lshep <lori.shepherd@roswellpark.org>
2026-04-27 13:03:14 -0400

    reverse close issue and post comment

--- scripts/validate.py
+++ scripts/validate.py
@@ -282,10 +282,11 @@ def main():
     # ----------------------------
     if action == "reopened":
         if not is_team_member(org, team_slug, actor):
-            close_issue()
             post_comment(
                 "⚠️ Only members of the PackageReview team can reopen this issue. Please request your issue be reopened on package-submission Bioconductor zulip channel."
             )
+            time.sleep(3)
+            close_issue()
             sys.exit(1)
         else:
             if has_label("inactive review"):
